f20de3d385124f55ab26ccc5c85de4c185d8e42b
parker-zim <pazimmerman@vassar.edu>
2018-12-05 22:13:56 -0500

    fixed us fucking up

--- my_zip.c
+++ my_zip.c
@@ -7,55 +7,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long FileLength = 0;
-char* buffer = 0;
+#define BUFF_SIZE (4*1024)
 
-int readFile(char const* path)
-{
-
-    FILE * f = fopen (path, "rb");
-
-    if (f) {
-      fseek (f, 0, SEEK_END);
-      FileLength = ftell (f);
-      fseek (f, 0, SEEK_SET);
-      buffer = (char*)malloc ((FileLength+1)*sizeof(char));
-      if (buffer){
-        if( 1!=fread( buffer , FileLength, 1 , f) )
-            fclose(f),free(buffer),fputs("entire read fails",stderr),exit(1);
-      }
-      fclose (f);
-    }
-    buffer[FileLength] = '\0';
-
-    return 0;
+FILE *openFile(char const* path){
+  FILE* f =fopen(path,"rb");
+  if (f){
+    return f;
+  } else{
+    fputs("file open failed",stderr),exit(1);
+  }
 }
 
-int zipBuf(){
-    char lastChar = buffer[0];
-    int numOccur = 1;
-    for (int c = 1; c <= FileLength; c ++){
-        if (buffer[c] == lastChar) {
-            numOccur++;
-        } else {
-            fwrite(&numOccur, sizeof(int), 1, stdout);
-            fwrite(&lastChar, sizeof(char), 1, stdout);
-            numOccur = 1;
 
-        }
-        lastChar = buffer[c];
-
-
+int zipFile(FILE * f){
+  char *buffer = (char*)malloc (BUFF_SIZE*sizeof(char));
+  int n = 0;
+  char lastChar = -1;
+  int numOccur = 0;
+  if (buffer){
+    while((n = fread(buffer, sizeof(char), BUFF_SIZE, f))){
+      if (!numOccur) {
+        numOccur = 1;
+        lastChar = buffer[0];
+      }
+      for (int c = 1; c <= n; c ++){
+          if (buffer[c] == lastChar) {
+              numOccur++;
+          } else{
+              fwrite(&numOccur, sizeof(int), 1, stdout);
+              fwrite(&lastChar, sizeof(char), 1, stdout);
+              numOccur = 1;
+          }
+          lastChar = buffer[c];
+      }
     }
+    if (numOccur > 1){
+      fwrite(&numOccur, sizeof(int), 1, stdout);
+      fwrite(&lastChar, sizeof(char), 1, stdout);
+    }
+  }
+    free(buffer);
     return 0;
 }
 
 int main(int argc, char *argv[]) {
-    if (readFile(argv[1]) == 0){
-        zipBuf();
-    };
-
-    free(buffer);
+  FILE *file = openFile(argv[1]);
+  zipFile(file);
+  fclose(file);
 
     return 0;
 }
