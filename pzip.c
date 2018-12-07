065219941392e308cdc2a0509d267bed471646a0
parker-zim <pazimmerman@vassar.edu>
2018-12-06 19:53:28 -0500

    add data structs for pzip

--- pzip.c
+++ pzip.c
@@ -1,7 +1,42 @@
 /*********************************************************
- * Your Name: 
- * Partner Name: 
+ * Your Name: Parker Zimmerman
+ * Partner Name: Yina Wang
  *********************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+// Data structure to hold both the character and its frequency
+struct combo {
+    int num;
+    char c;
+};
+
+// Data structure to hold the lock which will be used 
+struct fileInfo {
+    struct lock lock;
+    struct condition finished;    
+};
+
+int pzip() {
+    
+    return 0;
+}
 
 int main(int argc, char *argv[]) {
+    if (readFile(argv[1]) == 0){
+        pzipBuf();
+    };
+
+    //free(buffer);
+
+    return 0;
 }
+/*
+cant print in parallel
+break the file into certain size chunks based on memory usage
+break those chunks up based on number of threads
+mult threads working on reading file at once
+then use one thread to print chunk
+move onto next chunk once done
+*/
